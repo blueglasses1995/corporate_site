72c71e24bf3528d462274a743b9fc4f3a3f4d63b
Toshiki Matsukuma <blueglasses1995@gmail.com>
2024-10-05 21:56:38 +0700

    feat: graphql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
+import { AuthModule } from './modules/auth/auth.module';
+import { SharedModule } from './shared/shared.module';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.graphql'),
+      playground: process.env.ENV === 'dev',
+      context: ({ req }) => ({ request: req }),
+    }),
+    AuthModule,
+    SharedModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
